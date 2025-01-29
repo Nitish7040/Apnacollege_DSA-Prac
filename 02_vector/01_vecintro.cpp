fc2fe57765b3678531c406cd991781611c442133
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2025-01-29 20:37:23 +0530

    make initial changes

--- 02_vector/01_vecintro.cpp
+++ 02_vector/01_vecintro.cpp
@@ -6,56 +6,56 @@
 using namespace std;
 int main(){
 
-//  vector syntax
-// vector<datatype> name;
-// ex:- vector<int> vec = {1,2,3,4,5};
-// vector<int> vec(5, 0); // 5 elements with value 0
+ vector syntax
+vector<datatype> name;
+ex:- vector<int> vec = {1,2,3,4,5};
+vector<int> vec(5, 0); // 5 elements with value 0
 
-// vector<int> vec; // size=0
-// cout<<vec[0];
+vector<int> vec; // size=0
+cout<<vec[0];
 
-// vector<int> ve = {1,2,3,4,5};
-// cout<<ve[0];
-
-
-// vector<int> vec(5, 0); // 3->size, 0->value
-
-// vector<int> vec(5,0);
-// cout<<vec[0]<<endl;
-// cout<<vec[1]<<endl;
-// cout<<vec[2]<<endl;
-// cout<<vec[3]<<endl;
-// cout<<vec[4]<<endl;
+vector<int> ve = {1,2,3,4,5};
+cout<<ve[0];
 
 
+vector<int> vec(5, 0); // 3->size, 0->value
+
+vector<int> vec(5,0);
+cout<<vec[0]<<endl;
+cout<<vec[1]<<endl;
+cout<<vec[2]<<endl;
+cout<<vec[3]<<endl;
+cout<<vec[4]<<endl;
 
 
 
 
-                                               //////    FOR EACH LOOP  //////
-
-// vector<char> vec ={'a','b','c','d','e'};
-
-// for(char val : vec){
-//     cout<<val<<endl;
-// }
-
-//  In for each loop the [val or i] can go each index and print the value of that index......
 
 
+                                               ////    FOR EACH LOOP  //////
 
-                                        ///// CURD OPERATION IN VECTOR /////
-                    // for apply the curd operation in vector we can use some vector function like:-
-                       //  size
-                       //  push_back
-                       //  pop_back
-                       // insert
-                       // erase
-                       // clear
-                       // empty
-                       // front
-                       // back
-                       // at
+vector<char> vec ={'a','b','c','d','e'};
+
+for(char val : vec){
+    cout<<val<<endl;
+}
+
+ In for each loop the [val or i] can go each index and print the value of that index......
+
+
+
+                                        /// CURD OPERATION IN VECTOR /////
+                    for apply the curd operation in vector we can use some vector function like:-
+                        size
+                        push_back
+                        pop_back
+                       insert
+                       erase
+                       clear
+                       empty
+                       front
+                       back
+                       at
 
 //ex:-
 vector<char> vec ={'a','b','c','d','e'};
