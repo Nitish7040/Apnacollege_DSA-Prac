4e463062336626be8e995e80e83f9f40c764ff27
Nitish pandey <153537473+Nitish7040@users.noreply.github.com>
2025-02-04 10:10:31 +0530

    adding the 6,7,8 program of array

--- lovebabbar/Array_Vec/08 Linearsearch in 2D arr.cpp
+++ lovebabbar/Array_Vec/08 Linearsearch in 2D arr.cpp
@@ -1,11 +1,25 @@
 #include<iostream>
 using namespace std;
+
+// Founction for find the elements in 2d array .
+bool FindKey( int arr[][3], int row, int col, int key){
+     for(int i=0; i<row; i++){
+    for(int j=0; j<col;j++){
+       if(arr[i][j] == key)
+       return true;
+    }
+}
+      return false ;
+}
+
+
 int main(){
     
     // int arr[2][2]= {{1,2},{4,5}};
 int arr[3][3];
 int row=3;
 int col=3;
+
 
 //takin input from user with row wise:-
 
@@ -16,12 +30,22 @@ for(int i=0; i<row;i++){
 }
 
     // cout<<arr[0][2];
-cout<<"Row wise values print:-"<<endl;
+cout<<"your input 2D array:-"<<endl;
 for(int i=0; i<row; i++){
     for(int j=0; j<col;j++){
         cout<<arr[i][j]<<" ";
     }
     cout<<endl;
 }
+
+int key=18;
+
+// FindKey(arr, 3 , 3, 8);
+if(FindKey(arr, 3 , 3, key)){
+    cout<<key;
+}else{
+    cout<<"not found";
+}
+
    return 0;
 }
